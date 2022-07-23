774877568b82983d9e685dc0cdad77ead7137832
JunSeok <ppu1234@naver.com>
2022-07-23 22:12:51 +0900

    0723_commit

--- src/store/reducers/tempSlice.ts
+++ src/store/reducers/tempSlice.ts
@@ -10,17 +10,19 @@ const initialState: TempState = {
     count: 0,
 }
 
+//action과 reducer를 한번에 작성
 export const tempSlice = createSlice({
-    name: 'temp',
-    initialState,
+    name: 'temp', // slice의 이름
+    initialState, // 초기화
     reducers: {
+        // 객체로 반환, 모두 함수로 구성
         // Action
-        setTemp: (state, payload) => {
+        setTemp: (state, action) => {
             // setTemp라는 액션, 액션 발행 후 값 변화 . 단순한 JS 문법 가능, 시간 걸리는 비동기 작업은 안된다는 약간의 제약이 있다.
-            state.tempValue = payload.payload
+            state.tempValue = action.payload
         },
-        setCount: (state, payload) => {
-            state.count = payload.payload
+        setCount: (state, action) => {
+            state.count = action.payload
         },
     },
 })
@@ -28,3 +30,4 @@ export const tempSlice = createSlice({
 export const { setTemp, setCount } = tempSlice.actions
 
 export default tempSlice.reducer
+// store에 전달
